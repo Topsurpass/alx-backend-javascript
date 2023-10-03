83a462f60495775539b48bc8f1bcab091a0cbb7f
Olowosuyi Temitope <temitopeabiodun685@gmail.com>
2023-10-03 23:29:38 +0100

    resolve bug

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,15 +1,17 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const defaultGrade = { grade: 'N/A' };
-  if (Array.isArray(students)) {
-    const stdFilter = students.filter((student) => student.location === city);
-    return stdFilter.map((student) => ({
-      id: student.id,
-      firstname: student.firstName,
-      location: student.location,
-      grade: (newGrades
-        .filter((obj) => obj.studentId === student.id)
-        .pop() || defaultGrade).grade,
-    }));
-  }
-  return [];
+  const exactStudents = students.filter((student) => student.location === city);
+
+  const gradeStudents = exactStudents.map((student) => {
+    for (const theGrade of newGrades) {
+      if (student.id === theGrade.studentId) {
+        student.grade = theGrade.grade; // eslint-disable-line no-param-reassign
+      }
+    }
+    if (student.grade === undefined) {
+      student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+    }
+    return student;
+  });
+
+  return gradeStudents;
 }
