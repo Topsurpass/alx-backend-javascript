e67f7e7931da2b277a3b5606e101614e0a625e72
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-11-23 01:09:02 +0100

    used strictEqual

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,15 +3,15 @@ const calculateNumber = require("./0-calcul.js");
 
 describe("calculateNumber(a, b)", function () {
 	it("No floating number", function () {
-		assert.equal(calculateNumber(1, 3), 4);
+		assert.strictEqual(calculateNumber(1, 3), 4);
+		assert.strictEqual(calculateNumber(1.0, 2.0), 3);
 	});
 	it("should be rounded up", function () {
-		assert.equal(calculateNumber(1, 3.7), 5);
+		assert.strictEqual(calculateNumber(1, 3.7), 5);
+		assert.strictEqual(calculateNumber(2.5, 3.9), 7);
+		assert.strictEqual(calculateNumber(1.2, 3.7), 5);
 	});
-	it("should be rounded up", function () {
-		assert.equal(calculateNumber(1.2, 3.7), 5);
-	});
-	it("should be rounded up", function () {
-		assert.equal(calculateNumber(2.5, 3.9), 7);
+	it("should be rounded down", function () {
+		assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
 	});
 });
