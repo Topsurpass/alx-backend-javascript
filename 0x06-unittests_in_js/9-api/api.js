db74db1a9db9c3c06c9d7be8f177b5feadf92dd0
Temitope Olowosuyi <Temitope.Olowosuyi@etranzact.com>
2023-11-23 20:37:01 +0100

    regex integration testing

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (_, res) => {
+	res.statusCode = 200;
+	res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id(\\d+)", (req, res) => {
+	const id = req.params.id;
+	res.statusCode = 200;
+	res.send(`Payment methods for cart :${id}`);
+});
+
+app.listen(7865, () => {
+	console.log("API available on localhost port 7865");
+});
+
+module.exports = app;
